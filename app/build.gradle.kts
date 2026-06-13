b17743d3bc1e6962043d77bc1d8b236dca33eee9
Shardul Vikram Singh <shardulvikrm@gmail.com>
2026-06-14 03:22:15 +0530

    bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "SVS.pdfinspector"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.3.0"
+        versionCode = 8
+        versionName = "0.3.1"
     }
 
     dependenciesInfo {
