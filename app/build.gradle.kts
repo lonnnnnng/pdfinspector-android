18f2812db8822edea17afd91e08fd16d742c3444
guohailong <1105744725@qq.com>
2026-07-30 11:09:16 +0800

    发布 PDF 检查器 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.lonnnnnng.pdfinspector"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.4.2"
+        versionCode = 13
+        versionName = "0.5.0"
     }
 
     dependenciesInfo {
